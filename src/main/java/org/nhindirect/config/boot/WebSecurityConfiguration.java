61bc22d6b568b530b6676b8beb7dfeced11bad6f
gm2552 <gm2552@localhost>
2018-11-20 07:23:58 -0600

    Support bcrypt encryption of password in bootstrap properties.
    Also updating to Greenwich.M2.

--- src/main/java/org/nhindirect/config/boot/WebSecurityConfiguration.java
+++ src/main/java/org/nhindirect/config/boot/WebSecurityConfiguration.java
@@ -3,18 +3,19 @@ package org.nhindirect.config.boot;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @EnableWebSecurity
 @Configuration
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter
-{
+{	
+	private static final String BCRYPT_PREFIX = "{bcrypt}";
+	
 	@Value("${spring.security.user.name}")
 	protected String username;
 	
@@ -32,16 +33,22 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter
     	httpSecurity.csrf().disable();
     }   
     
-	
-    @Bean
-    public InMemoryUserDetailsManager inMemoryUserDetailsManager() 
+    
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception
     {
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        String encodedPassword = passwordEncoder().encode(password);
-        manager.createUser(User.withUsername(username).password(encodedPassword).roles("USER").build());
-        return manager;
-    } 	
-	
+    	if (password.startsWith(BCRYPT_PREFIX))
+    		password = password.substring(BCRYPT_PREFIX.length());  		
+    	else
+    		password = passwordEncoder().encode(password);
+
+        auth
+           .inMemoryAuthentication()
+           .passwordEncoder(passwordEncoder())
+           .withUser(username)
+           .password(password)
+           .roles("USER");
+    }
     
     @Bean
     public PasswordEncoder passwordEncoder() 
