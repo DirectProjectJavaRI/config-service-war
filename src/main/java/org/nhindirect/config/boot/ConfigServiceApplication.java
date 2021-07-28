1bc97c46c90f9b32b1444d7624df1bdeff1d3d24
gm2552 <gm2552@xmppserver.cernerdirectsupport.com>
2021-07-28 14:59:44 -0500

    Initial Direct 8.0.0 checkin.

--- src/main/java/org/nhindirect/config/boot/ConfigServiceApplication.java
+++ src/main/java/org/nhindirect/config/boot/ConfigServiceApplication.java
@@ -4,10 +4,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 
-@ComponentScan({"org.nhindirect.config", "org.nhindirect.config.boot"})
+@ComponentScan({"org.nhindirect.config"})
 @SpringBootApplication
+@EnableR2dbcRepositories("org.nhindirect.config.repository")
+@EnableScheduling
 public class ConfigServiceApplication extends SpringBootServletInitializer
 {	
 	@Override
